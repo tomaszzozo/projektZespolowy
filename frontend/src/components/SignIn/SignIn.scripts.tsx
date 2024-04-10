8f04a91f302e3a1e10bc6ff6a070e6d76b1b355e
Tomasz Wojtkiewicz <tomaszkuq@gmail.com>
2024-04-10 19:46:05 +0200

    Added mail and password validation

---
+++ frontend/src/components/SignIn/SignIn.scripts.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import * as EmailValidator from "email-validator";
+import { NavigateFunction } from "react-router-dom";
+
+export namespace SignInScripts {
+  export const onSubmit = (
+    event: React.FormEvent<HTMLFormElement>,
+    setEmailError: React.Dispatch<React.SetStateAction<string>>,
+    setPasswordError: React.Dispatch<React.SetStateAction<string>>,
+    navigate: NavigateFunction,
+  ) => {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    const email = data.get("email")!.toString().trim();
+    const password = data.get("password")!.toString().trim();
+
+    if (!EmailValidator.validate(email.trim())) {
+      setEmailError("Nieprawidłowy email");
+      return;
+    }
+    setEmailError("");
+
+    if (
+      !/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*()])[a-zA-Z1234567890!@#$%^&*()]{6,30}$/.test(
+        password,
+      )
+    ) {
+      setPasswordError("Nieprawidłowe hasło");
+      return;
+    }
+    setPasswordError("");
+
+    navigate("/");
+  };
+}
--- frontend/src/components/SignIn/SignIn.tsx
+++ frontend/src/components/SignIn/SignIn.tsx
@@ -1,21 +1,37 @@
-import React from "react";
-import { Avatar, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Avatar, Box, Button, TextField } from "@mui/material";
 import { Lock } from "@mui/icons-material";
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
+import { SignInScripts } from "./SignIn.scripts";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const navigate = useNavigate();
+
   return (
     <div
       className={
-        "fullscreenParent d-flex align-items-center justify-content-center m-0 p-0 bg-white"
+        "fullscreenParent d-flex align-items-center justify-content-center m-0 p-0"
       }
     >
       <div
         className={
-          "d-flex align-items-center justify-content-center w-75 h-70 shadow-lg border-radius-20 bg-secondary " + styles.card
+          "d-flex align-items-center justify-content-center w-75 h-70 shadow-lg border-radius-20 bg-secondary " +
+          styles.card
         }
       >
-        <div
+        <Box
+          component={"form"}
+          onSubmit={(event) =>
+            SignInScripts.onSubmit(
+              event,
+              setEmailError,
+              setPasswordError,
+              navigate,
+            )
+          }
           className={
             "w-90 h-90 container p-0 m-0 d-flex flex-column justify-content-between"
           }
@@ -27,7 +43,11 @@ export default function SignIn() {
               </Avatar>
             </div>
             <div className={"row pt-1"}>
-              <p className={"col d-flex justify-content-center fs-1 fw-bold m-0 color-black"}>
+              <p
+                className={
+                  "col d-flex justify-content-center fs-1 fw-bold m-0 color-black"
+                }
+              >
                 Logowanie
               </p>
             </div>
@@ -35,21 +55,36 @@ export default function SignIn() {
           <div className={"d-flex flex-column justify-content-center"}>
             <div className={"row p-2"}>
               <div className={"col d-flex justify-content-center"}>
-                <TextField label="Email" variant="outlined" />
+                <TextField
+                  label="Email"
+                  variant="outlined"
+                  name={"email"}
+                  error={emailError !== ""}
+                  helperText={emailError}
+                />
               </div>
             </div>
             <div className={"row p-2"}>
               <div className={"col d-flex justify-content-center"}>
-                <TextField label="Hasło" type="password" variant="outlined" />
+                <TextField
+                  label="Hasło"
+                  type="password"
+                  variant="outlined"
+                  name={"password"}
+                  error={passwordError !== ""}
+                  helperText={passwordError}
+                />
               </div>
             </div>
           </div>
           <div className={"row m-0"}>
             <div className={"col d-flex justify-content-center"}>
-              <Button variant="contained" size={"large"}>Zaloguj</Button>
+              <Button variant="contained" size={"large"} type={"submit"}>
+                Zaloguj
+              </Button>
             </div>
           </div>
-        </div>
+        </Box>
       </div>
     </div>
   );
