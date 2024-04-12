7d4aef9b1357acadf3216e3fd2847ea5bf033d00
Tomasz Wojtkiewicz <tomaszkuq@gmail.com>
2024-04-12 22:35:11 +0200

    Sign in - added show/hide password

--- frontend/src/endpoints/SignIn/SignIn.tsx
+++ frontend/src/endpoints/SignIn/SignIn.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
-import { Avatar, Box, Button, TextField } from "@mui/material";
-import { Lock } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { Lock, Visibility, VisibilityOff } from "@mui/icons-material";
 import styles from "./styles.module.scss";
 import { SignInScripts } from "./SignIn.scripts";
 import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const navigate = useNavigate();
@@ -56,6 +68,7 @@ export default function SignIn() {
             <div className={"row p-2"}>
               <div className={"col d-flex justify-content-center"}>
                 <TextField
+                  sx={{ width: "25ch" }}
                   label="Email"
                   variant="outlined"
                   name={"email"}
@@ -66,14 +79,39 @@ export default function SignIn() {
             </div>
             <div className={"row p-2"}>
               <div className={"col d-flex justify-content-center"}>
-                <TextField
-                  label="Hasło"
-                  type="password"
-                  variant="outlined"
-                  name={"password"}
-                  error={passwordError !== ""}
-                  helperText={passwordError}
-                />
+                <FormControl sx={{ width: "25ch" }} variant="outlined">
+                  <InputLabel
+                    htmlFor="outlined-adornment-password"
+                    className={passwordError === "" ? "" : "color-error-red"}
+                  >
+                    Hasło
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    name={"password"}
+                    type={showPassword ? "text" : "password"}
+                    error={passwordError !== ""}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={() => setShowPassword(!showPassword)}
+                          onMouseDown={(event) => event.preventDefault()}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Hasło"
+                  />
+                  <FormHelperText
+                    id="outlined-adornment-password-helper-text"
+                    className={passwordError === "" ? "" : "color-error-red"}
+                  >
+                    {passwordError}
+                  </FormHelperText>
+                </FormControl>
               </div>
             </div>
           </div>
