3a5abbeaa81d974755740695e64d6638cb1ad063
Tomasz Wojtkiewicz <tomaszkuq@gmail.com>
2024-06-21 20:01:50 +0200

    metoda POST dla użytkownika

--- backend/src/main/java/pl/pz/backend/backend/User.java
+++ backend/src/main/java/pl/pz/backend/backend/User.java
@@ -2,50 +2,52 @@ package pl.pz.backend.backend;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
 public class User {
-
     @Id
+    @Setter(AccessLevel.NONE)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @NotBlank(message = "")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()])[a-zA-Z0-9!@#$%^&*()]{8,30}", message = "")
     private String password;
-
     @Email
     @NotBlank(message = "")
     @Column(unique = true, nullable = false)
     private String email;
-
     @NotBlank(message = "")
     @Column(nullable = false)
     private String first_name;
-
     @NotBlank(message = "")
     @Column(nullable = false)
     private String last_name;
-
     @Column(nullable = false)
     private Boolean change_password = true;
-
     @NotBlank(message = "")
-    @Pattern(regexp = "^[0-9]{7,20}$", message = "")
+    @Pattern(regexp = "^\\d{7,20}$", message = "")
     @Column(unique = true, nullable = false, length = 20)
     private String phone_number;
-
     @Column(nullable = false)
     private Integer role = 0;
 
-    public User() {
+    public Dto toDto() {
+        return new Dto(id, email, first_name, last_name, phone_number, role);
     }
 
+    public record Dto(@Min(1) int id, @Email @NotBlank String email, @NotBlank String first_name,
+                      @NotBlank String last_name, @NotBlank @Pattern(regexp = "^\\d{7,20}$") String phone_number,
+                      @Min(0) int role) {
+    }
 }
--- backend/src/main/java/pl/pz/backend/backend/UserService.java
+++ backend/src/main/java/pl/pz/backend/backend/UserService.java
@@ -12,4 +12,13 @@ class UserService {
         return userRepository.findById(id).orElseThrow(() -> new UserDoesNotExistsException("User with given id %d does not exists".formatted(id)));
     }
 
+    void putUser(User.Dto dto) {
+        var user = userRepository.findById(dto.id()).orElseThrow(() -> new UserDoesNotExistsException("User with given id %d does not exists".formatted(dto.id())));
+        user.setEmail(dto.email());
+        user.setRole(dto.role());
+        user.setFirst_name(dto.first_name());
+        user.setLast_name(dto.last_name());
+        user.setPhone_number(dto.phone_number());
+        userRepository.save(user);
+    }
 }
